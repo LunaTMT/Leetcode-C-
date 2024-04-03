ddaabc4a2fa310513f9564e74f7902f2eaf4608c
Маршалл <44672093+LunaTMT@users.noreply.github.com>
2024-04-03 12:13:52 +0100

    Update 378. Kth Smallest Element in a Sorted Matrix.cpp

--- 378. Kth Smallest Element in a Sorted Matrix.cpp
+++ 378. Kth Smallest Element in a Sorted Matrix.cpp
@@ -1,3 +1,44 @@
+// Brilliant solution 
+class Solution {
+private:
+    void fast_io() const {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+public:
+    Solution() {
+        fast_io();
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        
+        if (k == n * n) return matrix[n - 1][n - 1];
+        if (k == 1) return matrix[0][0];
+        
+        int L = matrix[0][0], R = matrix[n - 1][n - 1];
+        while (L < R) {
+            int M = (L + R) >> 1;
+
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                count += upper_bound(matrix[i].begin(), matrix[i].end(), M) - matrix[i].begin();
+            }
+
+            if (count < k) {
+                L = M + 1;
+            } else {
+                R = M;
+            }
+        }
+
+        return R;
+    }
+};
+
+
+
+// easier heap sol
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
