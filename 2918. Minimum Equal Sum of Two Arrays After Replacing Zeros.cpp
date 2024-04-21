fb76b251198da772ab4ff388075a76428bc6e47d
Маршалл <44672093+LunaTMT@users.noreply.github.com>
2024-04-21 18:08:17 +0100

    Update 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp

--- 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp
+++ 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp
@@ -21,9 +21,8 @@ public:
                 n2_0 = true;
         }
 
-        if (!n1_0 || !n2_0 && n1 != n2)
-            if ((!n2_0 && n2 < n1) || (!n1_0 && n1 < n2))
-                return -1;
+        if ((!n2_0 && n2 < n1) || (!n1_0 && n1 < n2))
+            return -1;
 
         return n1 < n2? n2 : n1;
     }
