2512be70c6cfbc456b5e3ea34d9154c85c35d9ab
Маршалл <44672093+LunaTMT@users.noreply.github.com>
2024-04-21 18:14:24 +0100

    Update 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp

--- 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp
+++ 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp
@@ -1,8 +1,13 @@
 //This solution is absolutely disgusting
+// beats 100% with optimisation
 
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        
         double n1 = 0;
         double n2 = 0;
         
@@ -25,5 +30,35 @@ public:
             return -1;
 
         return n1 < n2? n2 : n1;
+    }
+};
+
+//Or a shortened way to for bool and int: 
+
+class Solution {
+public:
+    std::pair<long long, bool> calculateValues(const std::vector<int>& nums) {
+        long long sum = 0;
+        bool hasZero = false;
+        for (int n : nums) {
+            sum += (n ? n : 1);
+            if (n == 0)
+                hasZero = true;
+        }
+        return std::make_pair(sum, hasZero);
+    }
+
+    long long minSum(const std::vector<int>& nums1, const std::vector<int>& nums2) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+
+        auto [n1, n1_0] = calculateValues(nums1);
+        auto [n2, n2_0] = calculateValues(nums2);
+
+        if ((!n2_0 && n2 < n1) || (!n1_0 && n1 < n2))
+            return -1;
+
+        return n1 < n2 ? n2 : n1;
     }
 };
